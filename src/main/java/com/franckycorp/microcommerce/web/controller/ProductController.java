734fe3cc1987e1e6ae3bbfaa5d65d02369f62551
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-06-23 17:42:27 +0200

    Ajout du renvoi du bon code lors de la création de produit

--- src/main/java/com/franckycorp/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/franckycorp/microcommerce/web/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.franckycorp.microcommerce.web.controller;
 
 import com.franckycorp.microcommerce.web.dao.ProductDao;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.franckycorp.microcommerce.web.model.Product;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class ProductController {
@@ -27,7 +31,16 @@ public class ProductController {
     }
 
     @PostMapping("/Produits")
-    public void ajouterProduit(@RequestBody Product product) {
-        productDao.save(product);
+    public ResponseEntity<Product> ajouterProduit(@RequestBody Product product) {
+        Product productAdded = productDao.save(product);
+        if (Objects.isNull(productAdded)) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productAdded.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
